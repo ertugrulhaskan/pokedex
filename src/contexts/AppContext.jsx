82de0c31344dffc3ce552d31edc197e169c0978f
Ertugrul Alex Haskan <ertughaskan@gmail.com>
2022-09-19 20:55:29 -0700

    fetch pokemon list

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -1,14 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
+const POKEMON_API = `https://pokeapi.co/api/v2`;
+
 const AppContextProvider = ({ children }) => {
   const [filterTypeVisibility, setFilterTypeVisibility] = useState(false);
+  const [pokemonList, setPokemonList] = useState(null);
 
   const toggleFilterType = (e) => {
     e.preventDefault();
     setFilterTypeVisibility(!filterTypeVisibility);
   };
+
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch(`${POKEMON_API}/pokemon?limit=151`);
+      const data = await response.json();
+      setPokemonList(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
 
   return (
     <AppContext.Provider value={{ filterTypeVisibility, toggleFilterType }}>
