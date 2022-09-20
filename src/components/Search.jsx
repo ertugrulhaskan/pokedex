93e1e5c179b952e5b8c3852429b51604b56b6191
Ertugrul Alex Haskan <ertughaskan@gmail.com>
2022-09-19 20:14:44 -0700

    searchbox actions

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 // import PropTypes from 'prop-types'
 import { MdSearch, MdOutlineClear } from "react-icons/md";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+
   return (
     <form className="relative flex w-full flex-row">
       <button className="pointer-events-none absolute inset-y-0 left-3 text-xl text-zinc-400">
@@ -12,8 +14,14 @@ const Search = () => {
         className="w-full rounded-full bg-zinc-100 py-2 pl-10 pr-1 text-zinc-900 placeholder:text-zinc-400"
         type="text"
         placeholder="Search Pokemon"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
-      <button className="absolute top-1.5 right-2 flex h-7 w-7 flex-row items-center justify-center rounded-full text-center text-xl text-zinc-400 hover:bg-zinc-400 hover:text-white">
+      <button
+        type="button"
+        className="absolute top-1.5 right-2 flex h-7 w-7 flex-row items-center justify-center rounded-full text-center text-xl text-zinc-400 hover:bg-zinc-400 hover:text-white"
+        onClick={() => setQuery("")}
+      >
         <MdOutlineClear />
       </button>
     </form>
